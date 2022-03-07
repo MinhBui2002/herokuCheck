6d90e98f63d77f269d2dda96a7201581c6d893e0
MinhBui2002 <heribertschneller@gmail.com>
2022-03-07 09:29:11 +0700

    wafefawe

--- app.js
+++ app.js
@@ -10,19 +10,19 @@ const {
 } = require('./dbHandler');
 const app = express();
 
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true }));
-const path = require("path");
+const path = require('path');
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
 	res.render('all');
 });
 
-app.get("/insert", (req, res) => {
+app.get('/insert', (req, res) => {
 	res.render('create');
 });
 
-app.post("/edit", async (req, res) => {
+app.post('/edit', async (req, res) => {
 	const updateId = req.body.txtId;
 	const name = req.body.txtName;
 	const price = req.body.txtPrice;
@@ -38,39 +38,39 @@ app.post("/edit", async (req, res) => {
 		},
 	};
 	const myQuery = { _id: ObjectId(updateId) };
-	const collectionName = "Products";
+	const collectionName = 'Products';
 	await updateProduct(collectionName, myQuery, newvalues);
 	res.redirect('/all');
 });
 
-app.get("/delete", async (req, res) => {
+app.get('/delete', async (req, res) => {
 	const id = req.query.id;
-	const collectionName = "Products";
+	const collectionName = 'Products';
 	await deleteDocumentById(collectionName, id);
-	res.redirect("/all");
+	res.redirect('/all');
 });
 
-app.get("/all", async (req, res) => {
-	const collectionName = "Products";
+app.get('/all', async (req, res) => {
+	const collectionName = 'Products';
 	const result = await getCollection(collectionName);
-	res.render("all.hbs", { products: result });
+	res.render('all', { products: result });
 });
 
-app.get("/edit", async (req, res) => {
+app.get('/edit', async (req, res) => {
 	const id = req.query.id;
-	const collectionName = "Products";
+	const collectionName = 'Products';
 	const document = await getProductById(collectionName, id);
-	res.render("edit.hbs", { product: document });
+	res.render('edit', { product: document });
 });
 
-app.get("/view", async (req, res) => {
+app.get('/view', async (req, res) => {
 	const id = req.query.id;
-	const collectionName = "Products";
+	const collectionName = 'Products';
 	const document = await getProductById(collectionName, id);
-	res.render("find.hbs", { product: document });
+	res.render('find', { product: document });
 });
 
-app.post("/insert", async (req, res) => {
+app.post('/insert', async (req, res) => {
 	const name = req.body.txtName;
 	const price = req.body.txtPrice;
 	const picUrl = req.body.txtPicUrl;
@@ -81,9 +81,9 @@ app.post("/insert", async (req, res) => {
 		pic: picUrl,
 		description: des,
 	};
-	const collectionName = "Products";
+	const collectionName = 'Products';
 	await insertObject(collectionName, newP);
-	res.render("create.hbs");
+	res.render('create');
 });
 const port = process.env.PORT || 5000
 app.listen(port);
