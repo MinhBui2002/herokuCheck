44760c0bd8658a10853375e7d0da89a6e0cc3d72
MinhBui2002 <heribertschneller@gmail.com>
2022-03-08 21:07:07 +0700

    3roiewqpfi

--- dbHandler.js
+++ dbHandler.js
@@ -2,20 +2,19 @@ const { ObjectId } = require("mongodb");
 
 var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb+srv://Alex1:Minhbuiquang@cluster0.ufe9j.mongodb.net/test";
-const dbName = 'NoSQLBoosterSamples'
+const dbName = "NoSQLBoosterSamples";
 
-async function deleteDocumentById(collectionName,id){
-    let client = await MongoClient.connect(url)
-    let dbo = client.db(dbName) //GCH0904_DB: ten database
-    await dbo.collection(collectionName).deleteOne({_id: ObjectId(id)})
+async function deleteDocumentById(collectionName, id) {
+	let client = await MongoClient.connect(url);
+	let dbo = client.db(dbName);
+	await dbo.collection(collectionName).deleteOne({ _id: ObjectId(id) });
 }
 
 
-//myquery: dieu kien update(id=...); newvalues: gia tri moi can update
 async function updateProduct(collectionName, myquery, newvalues) {
-    let client = await MongoClient.connect(url)
-    let dbo = client.db(dbName) //GCH0904_DB: ten database
-    await dbo.collection(collectionName).updateOne(myquery, newvalues)
+	let client = await MongoClient.connect(url);
+	let dbo = client.db(dbName);
+	await dbo.collection(collectionName).updateOne(myquery, newvalues);
 }
 
 async function getProductById(collectionName, id) {
@@ -25,11 +24,10 @@ async function getProductById(collectionName, id) {
 }
 
 async function insertObject(collectionName, newP) {
-    let client = await MongoClient.connect(url)
-    let dbo = client.db(dbName) //GCH0904_DB: ten database
-    await dbo.collection(collectionName).insertOne(newP)
+	let client = await MongoClient.connect(url);
+	let dbo = client.db(dbName);
+	await dbo.collection(collectionName).insertOne(newP);
 }
-
 
 async function getCollection(collectionName) {
 	let client = await MongoClient.connect(url);
@@ -37,4 +35,10 @@ async function getCollection(collectionName) {
 	return await dbo.collection(collectionName).find({}).toArray();
 }
 
-module.exports = { insertObject, getCollection, getProductById, updateProduct,deleteDocumentById};
+module.exports = {
+	insertObject,
+	getCollection,
+	getProductById,
+	updateProduct,
+	deleteDocumentById,
+};
